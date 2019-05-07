9f235bc89a73a07a692664da45b2f7a51a9d0bbc
zheng <1582090156@qq.com>
2019-05-07 15:52:03 +0800

    6. 增加管理员模糊查询功能

---
+++ my-shop/my-shop-web-admin/target/my-shop-web-admin-1.0.0-SNAPSHOT/static/assets/app/validate.js
@@ -0,0 +1,39 @@
+/**
+ * 函数对象
+ * @type {{init}}
+ */
+var Validate = function () {
+    /**
+     * 初始化 jquery validation
+     */
+    var handlerValidation = function () {
+        //手机号码验证
+        $.validator.addMethod("mobile", function(value, element) {
+            var length = value.length;
+            var mobile = /^(13[0-9]{9})|(18[0-9]{9})|(14[0-9]{9})|(17[0-9]{9})|(15[0-9]{9})$/;
+            return this.optional(element) || (length == 11 && mobile.test(value));
+        }, "手机号码格式错误")
+
+
+        $("#inputForm").validate({
+            errorElement:'span',
+            errorClass: 'help-block',
+
+            errorPlacement: function (error, element) {
+                element.parent().parent().attr("class", "form-group has-error");
+                error.insertAfter(element);
+            }
+        });
+    }
+
+    return {
+        init: function () {
+            handlerValidation();
+        }
+    }
+
+}();
+
+$(document).ready(function () {
+    Validate.init();
+});
